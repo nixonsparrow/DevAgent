7e428d9c84aa77f67eb5f0faef79db0510b9e717
Nikodem Wróbel <nwrobel@bsbox.pl>
2023-05-24 23:38:03 +0200

    fix migrations

--- devagent/urls.py
+++ devagent/urls.py
@@ -3,11 +3,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from manager.views import error_404, error_403, error_500
+
 urlpatterns = [
     path("", include("users.urls")),
     path("", include("manager.urls")),
     path("admin/", admin.site.urls),
 ]
+
+handler403 = error_403
+handler404 = error_404
+handler500 = error_500
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
