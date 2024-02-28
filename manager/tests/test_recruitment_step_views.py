c25c8454c650917a8d33bb62c5e6554013e51432
Nikodem Wróbel <nikodem.wrobel@sportdata.pl>
2024-02-28 19:08:06 +0100

    Fix tests

--- manager/tests/test_recruitment_step_views.py
+++ manager/tests/test_recruitment_step_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from manager.models import RecruitmentStep
+from manager.models import RecruitmentStep, StepType
 from manager.tests import TestingBase
 from manager.views import RecruitmentStepCreateView, RecruitmentStepUpdateView
 
@@ -53,14 +53,15 @@ class RecruitmentStepUpdateViewTestCase(TestingBase, TestCase):
         self.assertEqual(response.status_code, 403)
 
     def test_form_valid(self):
+        new_step_type = StepType.objects.create(name="New test type", added_by=self.user)
         self.log_user()
         response = self.client.post(
             reverse("step-update", kwargs={"pk": self.step.id}),
-            data={"type": self.step_type.id},
+            data={"type": new_step_type.id},
         )
         self.assertEqual(response.status_code, 302)
-        step = RecruitmentStep.objects.latest("id")
-        self.assertNotEqual(step.id, self.step.id)
+        self.step.refresh_from_db()
+        self.assertEqual(self.step.type.id, new_step_type.id)
 
     def test_get_success_url(self):
         view = RecruitmentStepUpdateView()
